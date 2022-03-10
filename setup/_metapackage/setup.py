7d521eaa319ccddc77bc3f0ca2f90a0dea85c78c
dani <dani.xtendoo@gmail.com>
2022-03-10 11:55:42 +0100

    [ADD] Añadidos módulos de hr-holidays para odoo V12.0

---
+++ setup/_metapackage/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open('VERSION.txt', 'r') as f:
+    version = f.read().strip()
+
+setuptools.setup(
+    name="odoo13-addons-oca-hr-holidays",
+    description="Meta package for oca-hr-holidays Odoo addons",
+    version=version,
+    install_requires=[
+        'odoo13-addon-hr_holidays_credit',
+        'odoo13-addon-hr_holidays_leave_auto_approve',
+        'odoo13-addon-hr_holidays_leave_repeated',
+        'odoo13-addon-hr_holidays_natural_period',
+        'odoo13-addon-hr_holidays_public',
+        'odoo13-addon-hr_holidays_settings',
+        'odoo13-addon-hr_holidays_validity_date',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Odoo',
+    ]
+)
